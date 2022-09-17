68280dfdbb1092a90ab3853ddd5bc9be106bf8eb
Soncha19 <sofeeyahum@gmail.com>
2022-09-17 15:38:18 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from waitress import serve
+app = Flask(__name__)
+
+@app.route('/api/v1/hello-world-3')
+def hello_world():  # put application's code here
+    return "Hello world 3",200
+
+if __name__ == '__main__':
+    print("Server started")
+    serve(app)
